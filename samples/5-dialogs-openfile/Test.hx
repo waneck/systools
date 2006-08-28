7889083691ec4ac6c2784f255cf4b42c7b53592f
edwin.vanrijkom <edwin.vanrijkom@4e2c3aa4-5e1b-0410-9e24-bb80920d9600>
2006-08-28 20:20:18 +0000

    open file dialog sample
    git-svn-id: http://neko-systools.googlecode.com/svn/trunk@17 4e2c3aa4-5e1b-0410-9e24-bb80920d9600

---
+++ samples/5-dialogs-openfile/Test.hx
@@ -0,0 +1,8 @@
+import systools.Dialogs;
+
+class Test {
+		
+	static function main() {
+		trace(Dialogs.openFile("Select a file please!", "Please select one or more files, so we can see if this method works", "") );		
+	}
+}
