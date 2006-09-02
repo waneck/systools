6c2b3edc88440077a1340480feac778f483aca26
edwin.vanrijkom <edwin.vanrijkom@4e2c3aa4-5e1b-0410-9e24-bb80920d9600>
2006-09-02 11:21:03 +0000

    release 0.0.2
    git-svn-id: http://neko-systools.googlecode.com/svn/trunk@21 4e2c3aa4-5e1b-0410-9e24-bb80920d9600

--- make_haxelib.bat
+++ make_haxelib.bat
@@ -1,8 +1,24 @@
+REM -- main folder
 del systools
 mkdir systools
 copy haxelib.xml systools
-mkdir systools\systools
-xcopy api\systools systools\systools
+
+REM -- api:
+cd api\systools
+svn export . ..\..\systools\systools
+cd ..\..
+
+REM -- samples
+cd samples
+svn export . ..\systools\samples
+cd ..
+
+REM -- sources
+cd src
+svn export . ..\systools\src
+cd ..
+
+REM -- ndll's:
 mkdir systools\ndll
 mkdir systools\ndll\Windows
 copy bin\win\systools.ndll systools\ndll\Windows
