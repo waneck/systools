cfe2c235ecb9f63fb83330c0ceca07f457b209ee
eladin <eladin@4e2c3aa4-5e1b-0410-9e24-bb80920d9600>
2006-11-28 15:14:02 +0000

    actually use Dialogs.confirm for the confirm error test; trace the result of the confirm() calls.
    git-svn-id: http://neko-systools.googlecode.com/svn/trunk@39 4e2c3aa4-5e1b-0410-9e24-bb80920d9600

--- samples/3-dialogs/Test.hx
+++ samples/3-dialogs/Test.hx
@@ -4,8 +4,8 @@ class Test {
 		
 	static function main() {
 		Dialogs.message("neko-systools","Hello World!",false);
-		Dialogs.confirm("neko-systools","Please confirm?",false);
+		trace("confirm: "+Dialogs.confirm("neko-systools","Please confirm?",false));
 		Dialogs.message("neko-systools","Message error test", true);
-		Dialogs.message("neko-systools","Confirm error test", true);
+		trace("confirm error: "+Dialogs.confirm("neko-systools","Confirm error test", true));
 	}
 }
